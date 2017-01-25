35ed408fec5724d1e0cd39be610d95818c443b55
JunshengShen <mrokelin@hotmail.com>
2017-01-26 00:04:16 +0800

    added one layer to 3 hidden layers

--- main.m
+++ main.m
@@ -18,7 +18,8 @@ Y=(reshape(Y,400,m))';
 theta1=randn(400,401)./10;
 theta2=randn(400,401)./10;
 theta3=randn(400,401)./10;
-nn_params= [theta1(:) ; theta2(:);theta3(:)];% Unroll parameters 
+theta4=randn(400,401)./10;
+nn_params= [theta1(:) ; theta2(:);theta3(:);theta4(:)];% Unroll parameters 
 
 %% ================ Part 3: Compute Cost (Feedforward) ================
 lambda = 0;
@@ -33,7 +34,7 @@ nn_params=nn_params-grad.*0.01;
 [J grad] = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, ...
                    num_labels, X, Y, lambda);
 fprintf(['J= %f \n'], J); 
-for i=1:500
+for i=1:100
   nn_params=nn_params-grad.*0.01;
   [J grad] = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, ...
                    num_labels, X, Y, lambda);
@@ -53,8 +54,11 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):(2*(hidden_layer_size * (input_layer_size + 1)))), ...
                  hidden_layer_size, (hidden_layer_size + 1));
-Theta3 = reshape(nn_params((1 + 2*(hidden_layer_size * (input_layer_size + 1))):end), ...
+Theta3 = reshape(nn_params((1 + 2*(hidden_layer_size * (input_layer_size + 1))):(3*(hidden_layer_size * (input_layer_size + 1)))), ...
                  num_labels, (hidden_layer_size + 1));
+Theta4 = reshape(nn_params((1 + 3*(hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));            
+                 
 a1 = [ones(1, 1) TESTX]; 
  
 z2 = a1 * Theta1';  
@@ -65,8 +69,12 @@ z3 = a2 * Theta2';
 a3= sigmoid(z3);
 a3=[ones(1,1) a3];
 
-z4=a3 * Theta3'; 
-h = sigmoid(z4);
+z4 = a3 * Theta3'; 
+a4= sigmoid(z4);
+a4=[ones(1,1) a4];
+
+z5=a4 * Theta4'; 
+h = sigmoid(z5);
 h=reshape(h,20,20);
 h=h';
 imagesc(h),colorbar,colormap gray;
